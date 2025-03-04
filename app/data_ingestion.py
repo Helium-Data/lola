b432aba57ffa98de9f2f7fde053e3788adc8c162
sasuusen <osas.usen@gmail.com>
2025-03-04 01:00:55 +0100

    reset index - run nest reset index

--- app/data_ingestion.py
+++ app/data_ingestion.py
@@ -27,7 +27,7 @@ from llama_index.core.node_parser import SentenceSplitter
 from llama_index.readers.file import PDFReader, DocxReader
 
 from dotenv import load_dotenv
-from config import config
+from .config import config
 
 load_dotenv()
 
